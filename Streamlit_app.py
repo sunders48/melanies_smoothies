ca76f7790123f3fe905057a4777f7317c9333786
sunders48 <sunder.singh@hcltech.com>
2024-05-22 18:16:26 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -14,7 +14,7 @@ st.write('The Name on your smoothie is:',name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col(SEARCH_ON))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col(search_on))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
@@ -35,7 +35,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ''
 
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'search_on'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen +  'Nutrition Information')
