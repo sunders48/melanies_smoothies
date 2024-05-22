059ab1b92f6b9c513bc3f8a9ef70e522496c76b5
sunders48 <sunder.singh@hcltech.com>
2024-05-22 19:10:56 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -15,12 +15,12 @@ st.write('The Name on your smoothie is:',name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
 #convert the snowflake data frame into pandas data frame so we can use LDC function
-pd_df=my_dataframe.to_pandas()
+#pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
@@ -36,8 +36,8 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ''
 
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'search_on'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'search_on'].iloc[0]
+        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen +  'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
